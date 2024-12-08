c4b37a6fba9a8ff962b66bee1c6de3344a28bf2f
Nick Gillespie <nickgillespie2002@gmail.com>
2024-12-08 09:19:15 -0500

    Assignment 6 first commit

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -1,14 +1,37 @@
-import Database from "../Database/index.js";
-let { users } = Database;
+import model from "./model.js";
+import fs from 'fs';
+import path from 'path';
+
+// Define the log file path (relative to the script location)
+const logFilePath = path.join("./", 'app.log');
+
+// Logging function
+function log(input) {
+  // Format log entry with timestamp
+  const logEntry = `[${new Date().toISOString()}] ${input}\n`;
+
+  // Append the log entry to the log file
+  fs.appendFile(logFilePath, logEntry, (err) => {
+    if (err) {
+      console.error('Failed to write to log file:', err);
+    }
+  });
+}
+
 export const createUser = (user) => {
- const newUser = { ...user, _id: Date.now().toString() };
- users = [...users, newUser];
- return newUser;
+  delete user._id
+  return model.create(user);
 };
-export const findAllUsers = () => users;
-export const findUserById = (userId) => users.find((user) => user._id === userId);
-export const findUserByUsername = (username) => users.find((user) => user.username === username);
-export const findUserByCredentials = (username, password) =>
-  users.find( (user) => user.username === username && user.password === password );
-export const updateUser = (userId, user) => (users = users.map((u) => (u._id === userId ? user : u)));
-export const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
+export const findAllUsers = () => model.find();
+export const findUserById = (userId) => model.findById(userId);//model.findById(userId);
+export const findUserByUsername = (username) => model.findOne({ username: username });
+export const findUserByCredentials = (username, password) => model.findOne({ username, password });
+export const findUsersByRole = (role) => model.find({ role: role });
+export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const findUsersByPartialName = (partialName) => {
+  const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+  return model.find({
+    $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+  });
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import session from "express-session";
 import "dotenv/config";
-import express from 'express'
+import express from 'express';
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -9,6 +10,8 @@ import UserRoutes from "./Kanbas/Users/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
